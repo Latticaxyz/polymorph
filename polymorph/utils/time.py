3eb9a63756e65c3b061701112900168e9eb12e1c
wkflanders <wflan@seas.upenn.edu>
2025-12-14 03:28:33 -0500

    feat: expand time utils

--- polymorph/utils/time.py
+++ polymorph/utils/time.py
@@ -13,3 +13,20 @@ def months_ago(n: int) -> datetime:
     year = dt.year + (month - 1) // 12
     month = ((month - 1) % 12) + 1
     return dt.replace(year=year, month=month)
+
+
+def utc_ms() -> int:
+    return int(datetime.now(timezone.utc).timestamp() * 1000)
+
+
+def months_ago_ms(n: int) -> int:
+    dt = months_ago(n)
+    return int(dt.timestamp() * 1000)
+
+
+def datetime_to_ms(dt: datetime) -> int:
+    return int(dt.timestamp() * 1000)
+
+
+def ms_to_datetime(ms: int) -> datetime:
+    return datetime.fromtimestamp(ms / 1000, tz=timezone.utc)
